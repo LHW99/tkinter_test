8465b396f331f957e00af3738c22f330c2190ef9
haiduk <hwlee91@gmail.com>
2020-08-23 01:18:30 -0400

    viewer status bar

--- status.py
+++ status.py
@@ -16,6 +16,9 @@ my_img3 = ImageTk.PhotoImage(Image.open("/home/haiduk/Desktop/prime.png"))
 
 image_list = [my_img1, my_img2, my_img3]
 
+# status bar
+status = Label(root, text="Image 1 of " + str(len(image_list)), bd=1, relief=SUNKEN, anchor=E)
+
 my_label = Label(image=my_img1)
 my_label.grid(row=0, column=0, columnspan=3)
 
@@ -30,12 +33,16 @@ def forward(image_number):
   button_forward = Button(root, text="->", command=lambda: forward(image_number + 1))
   button_back = Button(root, text="<-", command=lambda: back(image_number - 1))
   
-  if image_number == 3:
+  if image_number == len(image_list):
     button_forward = Button(root, text="->", state=DISABLED)
 
   my_label.grid(row=0, column=0, columnspan=3)
   button_back.grid(row=1,column=0)
   button_forward.grid(row=1,column=2)
+
+  #update status bar
+  status = Label(root, text="Image " + str(image_number) + " of " + str(len(image_list)), bd=1, relief=SUNKEN, anchor=E)
+  status.grid(row=2, column=0, columnspan=3, sticky=W+E)
 
 def back(image_number):
   global my_label
@@ -54,6 +61,10 @@ def back(image_number):
   button_back.grid(row=1,column=0)
   button_forward.grid(row=1,column=2)
 
+  # update status bar
+  status = Label(root, text="Image " + str(image_number) + " of " + str(len(image_list)), bd=1, relief=SUNKEN, anchor=E)
+  status.grid(row=2, column=0, columnspan=3, sticky=W+E)
+
 # quit button
 button_quit = Button(root, text="Exit Program", command=root.quit)
 button_quit.grid(row=1, column=1)
@@ -63,5 +74,8 @@ button_back = Button(root, text="<-", command=back, state=DISABLED)
 button_back.grid(row=1,column=0)
 button_forward = Button(root, text="->", command=lambda: forward(2))
 button_forward.grid(row=1,column=2)
+
+# status bar position; 
+status.grid(row=2, column=0, columnspan=3, sticky=W+E)
 
 root.mainloop()
