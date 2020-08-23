4bb4d9255ef2bca6a76d408db33a9b239d72c2f0
haiduk <hwlee91@gmail.com>
2020-08-23 17:28:27 -0400

    finished popups

--- message.py
+++ message.py
@@ -1,10 +1,23 @@
 from tkinter import *
 from PIL import ImageTk, Image 
+from tkinter import messagebox
 
 root = Tk()
 root.title("Messages")
 
-b = Button(root, text="Click me!")
-b.pack()
+# function that makes a popup '.showinfo' is imported from messagebox
+# showinfo, showwarning, showerror, askquestion, askokcancel, askyesno
+def popup():
+  # (title of popup, content of popup)
+  response = messagebox.showinfo("This is my popup", "Hello World!")
+  Label(root, text=response).pack()
+  #if response == 1:
+  #  Label(root, text="You Clicked Yes!").pack()
+  #else:
+  # Label(root, text="You Clicked No!").pack()
+
+Button(root, text="Popup", command=popup).pack()
+
+
 
 root.mainloop()
