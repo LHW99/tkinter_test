d2ee1cfbe577041a7fb66153a2b8d18722c576f7
haiduk <hwlee91@gmail.com>
2020-08-30 00:42:24 -0400

    figuring out api

--- weather/weather.py
+++ weather/weather.py
@@ -1,10 +1,13 @@
 from tkinter import *
 from PIL import ImageTk, Image
+import requests
+import json
 
 root = Tk()
 root.title("Weather")
 root.geometry("500x500")
 
+api_request = requests.get("")
 
 
 root.mainloop()
