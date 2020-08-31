ae4d66b43b58d188520698818fbd43cbc0483dbb
haiduk <hwlee91@gmail.com>
2020-08-31 01:54:29 -0400

    rudimentary city lookup

--- weather/weather.py
+++ weather/weather.py
@@ -7,18 +7,26 @@ root = Tk()
 root.title("Weather")
 root.geometry("500x500")
 
+# create lookup function
+def cityLookup():
+  try:
+    api_request = requests.get("https://api.openweathermap.org/data/2.5/weather?q=" + city.get() + "&appid=82ccd7079a318bf163422706a0f39c8b")
+    # strip from json
+    api = json.loads(api_request.content)
+    name = api["name"]
+    weather = api["weather"][0]["description"]
+  except Exception as e:
+    api = "Error..."
 
-try:
-  api_request = requests.get("https://api.openweathermap.org/data/2.5/weather?q=vancouver&appid=82ccd7079a318bf163422706a0f39c8b")
-  # strip from json
-  api = json.loads(api_request.content)
-  name = api["name"]
-  weather = api["weather"]
-except Exception as e:
-  api = "Error..."
+  # put a 0 after the index for a nested dictionary; otherwise, python won't look in the dictionary
+  mylabel = Label(root, font=("Helvetica", 20), text=name + " currently " + weather)
+  mylabel.grid(row=0, column=0)
 
-# put a 0 after the index for a nested dictionary; otherwise, python won't look in the dictionary
-mylabel = Label(root, font=("Helvetica", 20), text=api["name"] + " currently " + api["weather"][0]["description"])
-mylabel.pack()
+city = Entry(root)
+city.grid(row=1, column=0)
+
+citybtn = Button(root, text="Look up City", command=cityLookup)
+citybtn.grid(row=1, column=1)
+
 
 root.mainloop()
